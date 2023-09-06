677f3fe845d3d78fa1f9a5def0d1f103e8354654
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2023-09-06 16:30:31 -0600

    Update branch

--- fe-house-p/src/app/layout.js
+++ fe-house-p/src/app/layout.js
@@ -1,20 +1,22 @@
-import '../styles/globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/misc/Navbar'
+import "../styles/globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/misc/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Campechanos app',
-  description: 'Campechanos app',
-}
+  title: "Housea",
+  description:
+    "La propuesta de Housea busca revolucionar la forma en que los inversionistas y las constructoras abordan el mercado inmobiliario. Al combinar la predicción de inversiones, la intermediación eficiente, la optimización de proyectos y el acceso a inversiones de créditos, se crea un ecosistema completo que maximiza las oportunidades y minimiza los riesgos para todas las partes involucradas.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="esp">
       <body className={inter.className}>
-        <Navbar/>
-        {children}</body>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
