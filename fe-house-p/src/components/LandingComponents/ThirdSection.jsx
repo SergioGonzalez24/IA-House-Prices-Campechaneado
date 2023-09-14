d8ab14ab0281e9c1a491d2d4d85e8f00733d0d1e
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2023-09-13 19:27:12 -0600

    Actiualizacion de seccion 3 animacion

--- fe-house-p/src/components/LandingComponents/ThirdSection.jsx
+++ fe-house-p/src/components/LandingComponents/ThirdSection.jsx
@@ -1,8 +1,35 @@
+'use client';
+
 import { NeuralAnimation } from "./LandingAnimation";
-export default function ThirdSection () {
-    return (
-        <>
-        <NeuralAnimation />
-        </>
-    );
+import React, { useState } from 'react';
+import { Box, Grid, Typography, Button, Container } from "@mui/material";
+
+export default function ThirdSection() {
+  const [showContent, setShowContent] = useState(false);
+
+  return (
+    <div style={{border: "2px solid black", overflow:"hidden", paddingTop:"16px"}}>
+      {showContent && (
+        <div style={{border: "2px solid black", overflow:"hidden",paddingTop:"16px"}}>
+             <Box
+      sx={{
+        height: "100%",
+        overflow: "hidden",
+        position: "relative",
+        marginTop: "90px",
+        marginBottom: "1px",
+        marginLeft: "30px",
+        padding: "10px",
+        paddingBottom: "100px",
+  
+      }}
+    >
+        <h1>SDFGHJKL</h1>
+    </Box>
+          
+        </div>
+      )}
+      <NeuralAnimation onAnimationComplete={() => setShowContent(true)} />
+    </div>
+  );
 }
