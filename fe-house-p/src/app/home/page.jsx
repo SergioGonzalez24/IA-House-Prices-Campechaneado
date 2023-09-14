bab14f2223d4878ba1bd5c4d9697f74bff4df99c
Antonio Oviedo <antoniooviedo131102@gmail.com>
2023-09-14 14:42:41 -0600

    final pagina

--- fe-house-p/src/app/home/page.jsx
+++ fe-house-p/src/app/home/page.jsx
@@ -1,8 +1,25 @@
 import Link from "next/link";
+import { PageContainer } from "@/components/AppComponents";
+import { ProjectCard } from "@/components/ProjectCard";
+import proyectos from "@/assets/data/proyectos";
+import {AiOutlineSearch} from "react-icons/ai"
+
 
 
 export default function Home() {
-    return (
-        <Link href="/home/1">Hola</Link>
-    );
+  return (
+    <PageContainer>
+      <h1 className="mb-2 text-[1.5rem]">Encuentra el proyecto que mejor se adapte a tus necesidades de inversion</h1>
+      <div className="mb-10 rounded-full px-5 py-3 text-[1.3rem] bg-[#5FA34A] text-white font-bold">
+        <AiOutlineSearch></AiOutlineSearch>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+        {
+          Object.keys(proyectos).map((key) => {
+            return <ProjectCard key={key} id={key} data={proyectos[key]}></ProjectCard>
+          })
+        }
+      </div>
+    </PageContainer>
+  );
 }
--- fe-house-p/src/components/AppComponents/PageContainer.jsx
+++ fe-house-p/src/components/AppComponents/PageContainer.jsx
@@ -2,7 +2,7 @@
 
 export default function PageContainer({children}) {
   return (
-    <div className="bg-gray-100 px-[20px] py-[20px] lg:px-[100px] md:py-[50px]">
+    <div className="bg-gray-100 px-[20px] py-[10px] lg:px-[100px] md:py-[30px]">
       {children}
     </div>
   )
