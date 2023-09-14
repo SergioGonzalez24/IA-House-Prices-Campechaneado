7947d68353b515227912f1d0361bbb7243964172
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2023-09-14 03:26:34 -0600

    Modificaciones y avance en upload proyect

---
+++ fe-house-p/src/app/agregarproyecto/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UploadProyects } from '@/components/UploadProyects';
+
+export default function TestPage() {
+    return (
+        <div>
+            <UploadProyects />
+        </div>
+    );
+}
