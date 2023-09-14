d10cc2e2b9054cb513113e3b9aa1e053ecb528c8
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2023-09-14 15:08:35 -0600

    actualizacion de imagen

--- fe-house-p/src/assets/images/index.jsx
+++ fe-house-p/src/assets/images/index.jsx
@@ -6,4 +6,5 @@ import backF1Sec from './backF1Sec.svg'
 import google from './google.png'
 import appstore from './appstore.png'
 import logo_housea from './logo.png'
-export { meme, en_construccion, degradado, phone_035, backF1Sec, google, appstore, logo_housea};
+import iphn from './iphn.png'
+export { meme, en_construccion, degradado, phone_035, backF1Sec, google, appstore, logo_housea, iphn};
--- fe-house-p/src/components/LandingComponents/FirstSection.jsx
+++ fe-house-p/src/components/LandingComponents/FirstSection.jsx
@@ -9,6 +9,7 @@ import {
   phone_035,
   google,
   appstore,
+  iphn,
 } from "@/assets/images";
 
 
@@ -173,14 +174,16 @@ export default function FirstSection() {
               muted
             />
             <Image
-              src={phone_035}
+              src={iphn}
+              // src={phone_035}
               alt="phone"
               style={{
                 position: "absolute", // Establece la posición absoluta para superponer la imagen de "phone"
                 top: 0, // Alinea la parte superior de "phone" con la parte superior del contenedor
-                left: -100, // Alinea la parte izquierda de "phone" con la parte izquierda del contenedor
-                width: "90%", // Ajusta el ancho de "phone" al 100% para que coincida con el contenedor
-                height: "auto", // Permite que la altura se ajuste automáticamente para mantener la proporción
+                left: 100, // Alinea la parte izquierda de "phone" con la parte izquierda del contenedor
+                //right: 10,
+                width: "auto", // Ajusta el ancho de "phone" al 100% para que coincida con el contenedor
+                height: "100%", // Permite que la altura se ajuste automáticamente para mantener la proporción
                 marginTop: "0px",
                 marginBottom:"1px"
                 
